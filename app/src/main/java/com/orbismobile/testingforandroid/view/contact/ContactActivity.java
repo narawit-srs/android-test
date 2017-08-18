a913201794af5fc3107b98058f0343a179332a00
Carlos Leonardo Camilo <carlosleonardovargashuaman@gmail.com>
2017-08-17 20:05:00 -0500

    Practicing with ui test

--- app/src/main/java/com/orbismobile/testingforandroid/view/contact/ContactActivity.java
+++ app/src/main/java/com/orbismobile/testingforandroid/view/contact/ContactActivity.java
@@ -45,7 +45,7 @@ public class ContactActivity extends AppCompatActivity implements ContactAdapter
         contactAdapter.setOnContactItemClickListener(this);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
